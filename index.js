035ebfed7b1c923b99a7965ade28b32362ebb3dd
radiation22 <radiationcorporation2@gmail.com>
2022-11-29 15:30:53 +0600

    update review

--- index.js
+++ index.js
@@ -118,17 +118,9 @@ async function run() {
       res.send(result);
     });
     // find user specific reviews
-    app.get("/myReviews", verifyJwt, async (req, res) => {
-      const decoded = req.decoded;
-      if (decoded.email !== req.query.email) {
-        res.status(403).send({ message: "forbidden" });
-      }
-      let query = {};
-      if (req.query.email) {
-        query = {
-          email: req.query.email,
-        };
-      }
+    app.get("/myReviews", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
       const cursor = reviewCollection.find(query);
       const reviews = await cursor.toArray();
       res.send(reviews);
